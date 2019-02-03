b5f4859d77fb350a99bee3b212d3bcfbefabbe83
CrabeMan <q_leprat@yahoo.fr>
2019-02-03 20:35:22 +0100

    Define Api IP and Port as BuildConfig field

--- android/app/src/main/java/fr/esgi/ransomware/ApiService.java
+++ android/app/src/main/java/fr/esgi/ransomware/ApiService.java
@@ -11,7 +11,7 @@ import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface ApiService {
-    String BASE_URL = "http://10.0.2.2:5000";
+    String BASE_URL = "http://" + BuildConfig.SERVER_IP + ":" + BuildConfig.SERVER_PORT;
 
     @POST("mobile")
     Call<Register.Response> register(@Body ApiService.Register register);
